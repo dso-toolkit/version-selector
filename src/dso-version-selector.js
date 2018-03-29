a56ea19467cade77f3a19edaceb39fb6e6ca1b18
Thomas Rijsewijk <thomas@infoprojects.nl>
2018-03-29 11:36:01 +0200

    #1: Initial commit

---
+++ src/dso-version-selector.js
@@ -0,0 +1,70 @@
+(function () {
+  'use strict';
+  // Get list of versions from versions.json
+  var selectedVersion = window.location.pathname.slice(1, -1);
+
+  var versionRequest = new XMLHttpRequest();
+  versionRequest.responseType = 'json';
+  versionRequest.addEventListener('load', createSelector);
+  versionRequest.open('GET', '/version-selector.json');
+  versionRequest.send();
+
+  function createSelector () {
+    // Create input select template
+    var jsonVersions = this.response;
+
+    var selectTemplate = document.createElement('select');
+    selectTemplate.setAttribute('id', 'dsoVersionSelector');
+    selectTemplate.setAttribute('class', 'dso-version-selector');
+    selectTemplate.setAttribute('onchange', 'openVersion()');
+
+    jsonVersions.versions.reduce(function (branches, item) {
+      var branchLabel = item.branch || item.version.substr(0, 3) + '.x';
+      var branch = branches.filter(function (r) {
+        return r.label === branchLabel;
+      })[0];
+
+      if (!branch) {
+        branch = {
+          label: branchLabel,
+          versions: []
+        };
+
+        branches.push(branch);
+      }
+      
+      branch.versions.push(item);
+      
+      return branches;
+    }, [])
+    .forEach(function (branch) {
+      var optGroup = document.createElement('optgroup');
+      optGroup.setAttribute('label', branch.label);
+
+      branch.versions.forEach(function (v) {
+        var option = document.createElement('option');
+        option.setAttribute('value', v.version);
+
+        if (v.version === selectedVersion) {
+          option.setAttribute('selected', true);
+        }
+
+        option.appendChild(document.createTextNode(v.version));
+        optGroup.appendChild(option);
+      });
+
+      selectTemplate.appendChild(optGroup);
+    });
+
+    // Insert select in .Header
+    var header = document.querySelector('.Header');
+    header.appendChild(selectTemplate);
+
+    // Open correct version
+    window.openVersion = function () {
+      var version = selectTemplate.options[selectTemplate.selectedIndex].value;
+
+      window.location.href='/' + version;
+    };
+  }
+})();
